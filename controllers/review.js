aa2a606507bfde9ba071b3b701d9979a5d6d37a4
richasinghkk <146985975+richasinghkk@users.noreply.github.com>
2025-04-25 01:28:44 +0530

    Fix: Add missing reviews.js controller

---
+++ controllers/review.js
@@ -0,0 +1,21 @@
+const Review = require("../models/review.js");
+const Listing = require("../models/listing.js");
+
+module.exports.createReview = async (req, res) => {
+    const listing = await Listing.findById(req.params.id);
+    const review = new Review(req.body.review);
+    review.author = req.user._id;
+    listing.reviews.push(review);
+    await review.save();
+    await listing.save();
+    req.flash("success", "New review created!");
+    res.redirect(`/listings/${listing._id}`);
+};
+
+module.exports.destroyReview = async (req, res) => {
+    const { id, reviewId } = req.params;
+    await Listing.findByIdAndUpdate(id, { $pull: { reviews: reviewId } });
+    await Review.findByIdAndDelete(reviewId);
+    req.flash("success", "Review deleted!");
+    res.redirect(`/listings/${id}`);
+};
